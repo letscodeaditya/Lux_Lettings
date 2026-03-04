31c2180dd4e6feb321337d32c747fa07564ed422
letscodeaditya <whyayush24@gmail.com>
2026-03-04 15:13:37 +0530

    adminn

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -55,6 +55,12 @@ export default function Footer() {
           navigate("/admin/login");
         }}>admin</a>
             </li>
+            <li>
+              <a href="" onClick={(e) => {
+          e.preventDefault();
+          navigate("/admin/create-temp");
+        }}>admin</a>
+            </li>
           </ul>
         </div>
 

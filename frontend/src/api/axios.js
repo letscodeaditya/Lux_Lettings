2da956648dabe84dc330806e9a893bbfee772dee
letscodeaditya <whyayush24@gmail.com>
2026-03-04 23:11:01 +0530

    fix

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'https://lux-lettings-backend.vercel.app',
+  baseURL: 'https://lux-lettings-backend.vercel.app',
 
-  baseURL: 'http://localhost:3010',
+  // baseURL: 'http://localhost:3010',
 });
 
 export default api;
